b91b1cd01d71186183f6a5e609dd008c49eb40df
Savya Vats <savyavats@gmail.com>
2026-07-23 20:32:00 -0400

    init

--- ChipExchange.java
+++ ChipExchange.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class ChipExchange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int var1 = scanner.nextInt();
-        System.out.println(var1);
+        int T = scanner.nextInt();
+        
+
     }
 }
