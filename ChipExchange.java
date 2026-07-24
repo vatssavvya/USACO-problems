ba3db1968a2530b7710723a2ed2b803c913dee2f
Savya Vats <savyavats@gmail.com>
2026-07-23 20:53:49 -0400

    long

--- ChipExchange.java
+++ ChipExchange.java
@@ -3,7 +3,14 @@ public class ChipExchange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt();
-        
+        for (int i = 0; i < T; i++) {
+            long A = scanner.nextLong();
+        }
+
+
+    }
+
+    public void solve(int A, int B, int cA, int cB, int fA) {
 
     }
 }
