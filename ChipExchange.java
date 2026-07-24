7327985f0b5d5d5ae82dd08485a1225741ef43e6
Savya Vats <savyavats@gmail.com>
2026-07-23 20:54:18 -0400

    init inputs

--- ChipExchange.java
+++ ChipExchange.java
@@ -5,6 +5,10 @@ public class ChipExchange {
         int T = scanner.nextInt();
         for (int i = 0; i < T; i++) {
             long A = scanner.nextLong();
+            long B = scanner.nextLong();
+            long cA = scanner.nextLong();
+            long cB = scanner.nextLong();
+            long fA = scanner.nextLong();
         }
 
 
